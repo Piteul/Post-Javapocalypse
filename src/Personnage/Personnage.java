acb1cfa1af392bcf7b9b80343d473722a5460b24
Unknown <moulardvalentin@gmail.com>
2017-12-18 17:11:02 +0100

    Ajout de fonctions de gestion de l'inventaire
    afficher inventaire
    ajouter objet
    jeter objet
    trouver une place libre dans l'inventaire

--- src/Personnage/Personnage.java
+++ src/Personnage/Personnage.java
@@ -1,5 +1,7 @@
 package Personnage;
 
+import java.util.Scanner;
+
 import Objet.Arme;
 import Objet.Objet;
 import Objet.ObjetVital;
@@ -23,7 +25,7 @@ public class Personnage extends Humain {
 	int endurance = 50;
 	Objet inventaire[] = new Objet[10];
 	Arme arme = new Arme("Couteau");
-
+	private Scanner sc;
 	/**
 	 * Constructeur
 	 */
@@ -44,12 +46,71 @@ public class Personnage extends Humain {
 	public void fouiller() {
 
 	}
-
+	
+	//visualisation de l'inventaire
 	public void voirInventaire() {
-
+		System.out.println("Contenu de l'inventaire");
+		for (int i =0; i < inventaire.length; i++) {
+			if (inventaire[i] != null) {
+				System.out.println(i + " - " + inventaire[i]);
+			}
+		}
+	}
+	//fonction d'ajout d'un objet dans l'inventaire
+	public void ajouterInventaire(Objet o) {
+		int i = placeLibre();
+		if (i != -1) {
+			inventaire[i] = o;
+		}
+	}
+	
+	//fonction pour retirer un objet de l'inventaire et liberer la place
+	public void jeterObjet() {
+		int action;
+		int nombre;
+		System.out.println("Quel objet voulez vous jetez ?");
+		sc = new Scanner(System.in);
+		action = sc.nextInt();
+		//si la quantité de l'objet qu'on veut supprimé est incorrect on redemande
+		if (action < 0 || action > inventaire.length) {
+			System.out.println("Emplacement inaccesible de l'inventaire");
+			jeterObjet();
+		//s'il existe un objet avec un quantité supérieur à 1, on demande, combien d'objet retirer
+		}else if(inventaire[action].getQuantite() > 1){
+			System.out.println("Combien voulez vous en retirer ?");
+			nombre = sc.nextInt();
+			while (nombre < 0 || nombre > inventaire[action].getQuantite()) {
+				System.out.println("Entrez une valeur correct");
+				nombre = sc.nextInt();
+				System.out.println("Vous avez retiré " + nombre + " " + inventaire[action].getNom() + " !");
+			}
+			inventaire[action].setQuantite(inventaire[action].getQuantite() - nombre);
+			if (inventaire[action].getQuantite()==0) {
+				System.out.println(inventaire[action] + " a été retiré !");
+				inventaire[action]=null;
+			}
+		//si l'objet n'existe qu'en un seul exemplaire, on le supprime simplement
+		}else{
+			System.out.println(inventaire[action] + " a été retiré !");
+			inventaire[action]=null;
+			
+		}
+	}
+	
+	//renvoie l'indice d'une place libre dans l'inventaire
+	public int placeLibre() {
+		int i = 0;
+		int indice = -1;
+		while (inventaire[i] != null && i < inventaire.length) {
+			i++;
+		}
+		indice = i;
+		if (indice == -1) {
+			System.out.println("L'inventaire est plein !");
+		}
+		return indice;
 	}
 
-	// dans l'inventaire
 
 	// a specifier
 	public void utiliserObjet() {
@@ -59,10 +120,6 @@ public class Personnage extends Humain {
 	public void seSoigner(ObjetVital p_objetVital) {
 		pointVie += p_objetVital.getPv_rendu();
 		endurance += p_objetVital.getEndurance_rendue();
-	}
-
-	public void jeterObjet() {
-
 	}
 
 	
